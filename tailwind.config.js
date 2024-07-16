be638cc016029580d443b8cf81fe307a3803348d
Rufat01 <rufatmustafayev04@gmail.com>
2024-07-16 16:03:41 +0400

    First version of site

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'sm': {'min': '512px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      'lg': {'min': '1024px'},
+      // => @media (min-width: 1024px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
