4a0bcbd8b7c3246a67498ffa4f4ee039aae1e5eb
Rufat01 <rufatmustafayev04@gmail.com>
2024-07-17 14:37:20 +0400

    Hero created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,14 +9,14 @@ const Header = () => {
     const [openMenu, setOpenMenu] = useState(false)
 
   return (
-    <div className="h-[100px]">
+    <div className="h-[100px] sm:h-[78px] md:h-[105px]">
       <div className="flex justify-between items-center py-8 px-5 sm:py-5 sm:border border-gray-200">
         <div>
           <a href="#">
             <img src={Logo} alt="" />
           </a>
         </div>
-        <nav className="px-[116px] sm:hidden">
+        <nav className="px-[116px] sm:hidden md:hidden">
           <ul className="flex items-center gap-10">
             <a href="" className="text-[17px] hover:text-[#4B6BFB] transition-all duration-300">Home</a>
             <a href="" className="text-[17px] hover:text-[#4B6BFB] transition-all duration-300">Blog</a>
@@ -27,7 +27,7 @@ const Header = () => {
         </nav>
         <div className="flex items-center gap-10 sm:gap-3">
             <div className="relative">
-                <input type="text" className="w-[166px] bg-[#F4F4F5] rounded-md pl-[10px] pr-[30px] py-2 sm:hidden" placeholder="Search"/>
+                <input type="text" className="w-[166px] md:w-[350px] bg-[#F4F4F5] rounded-md pl-[10px] pr-[30px] py-2 sm:hidden" placeholder="Search"/>
                 <img src={Search} className="absolute top-[9px] w-[22px] right-2 sm:static sm:w-8" onClick={() => setSearch(t => !t)} alt="" />
             </div>
             <div className="w-12 h-7 bg-[#F4F4F5] rounded-full p-[3px] sm:hidden" onClick={() => setActive(open => !open)}> 
@@ -43,7 +43,7 @@ const Header = () => {
             </div>
         </div>
 
-        <nav className={`${openMenu ? 'w-full' : 'w-0 overflow-x-hidden'} lg:hidden absolute right-0 top-[77px] transition-all duration-300 bg-gray-100/80 backdrop-blur-sm`}>
+        <nav className={`${openMenu ? 'w-full' : 'w-0 overflow-x-hidden'} lg:hidden absolute right-0 top-[77px] md:top-[104px] transition-all duration-300 bg-gray-100/80 backdrop-blur-sm z-20`}>
           <ul className="flex items-center flex-col gap-10 py-5">
             <a href="" className="text-[17px] w-full text-center hover:text-[#4B6BFB] transition-all duration-300">Home</a>
             <a href="" className="text-[17px] w-full text-center hover:text-[#4B6BFB] transition-all duration-300">Blog</a>
@@ -54,8 +54,8 @@ const Header = () => {
         </nav>
 
       </div>
-      <div className="lg:hidden px-5">
-         <input type="text" className={`${search ? 'animate__slideInDown block' : 'hidden'} animate__animated transition-all w-full bg-[#F4F4F5] z-10 rounded-md pl-[10px] pr-[30px] py-2 animate__animated`} placeholder="Search"/>
+      <div className="lg:hidden md:hidden px-5 z-10">
+         <input type="text" className={`${search ? 'animate__slideInDown block' : 'hidden'} animate__animated transition-all w-full bg-[#F4F4F5] rounded-md pl-[10px] pr-[30px] py-2 mt-5 animate__animated`} placeholder="Search"/>
       </div>
     </div>
   );
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import HeroImg from '../assets/Hero.png'
+import User from '../assets/User.png'
+
+const Hero = () => {
+  return (
+    <div className='relative w-full rounded-xl -z-10'>
+      <img src={HeroImg} className='w-full object-cover rounded-xl sm:h-[250px] sm:rounded-none md:rounded-none' alt="" />
+      <div className='absolute flex flex-col max-w-[600px] rounded-xl bg-white p-10 bottom-[-50px] shadow-md left-16 md:w-[450px] sm:w-[350px] sm:relative sm:top-[-80px] sm:left-5 sm:p-7'>
+          <span className='mb-4 w-[95px] text-center py-1 px-[10px] rounded-md text-sm text-white bg-[#4B6BFB] inline-block'>Technology</span>
+          <h1 className='mb-6 text-4xl sm:text-lg md:text-xl font-semibold'>The Impact of Technology on the Workplace: How Technology is Changing</h1>
+          <div className='flex items-center gap-5 sm:flex-col sm:items-start'>
+            <div className='flex items-center gap-3'>
+                <img src={User} alt="" />
+                <h4 className='text-[#97989F] sm:text-sm'>Jason Francisco</h4>
+            </div>
+            <span className='text-[#97989F] sm:text-sm'>August 20, 2024</span>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
