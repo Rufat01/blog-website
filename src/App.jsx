6f1ef54c5d5bd08c5aacba2dbfdca1a20a87dd0f
Rufat01 <rufatmustafayev04@gmail.com>
2024-07-19 20:15:49 +0400

    Post created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import 'animate.css';
 import Hero from "./components/Hero";
+import Posts from "./components/Posts";
 
 const App = () => {
   return (
     <div className="max-w-[1440px] my-0 mx-auto">
         <Header />
         <Hero />
+        <Posts />
     </div>
   )
 }
